6a5b262981be570f15eeba3ce93c8c30f368a182
skorpion <savinov_vu@inbox.ru>
2018-02-11 22:26:04 +0800

    implement fade-in-out animation.

--- src/app/directives/get-size.directive.spec.ts
+++ src/app/directives/get-size.directive.spec.ts
@@ -2,7 +2,7 @@ import { DrawWindowDirective } from './get-size.directive';
 
 describe('DrawWindowDirective', () => {
   it('should create an instance', () => {
-    const directive = new DrawWindowDirective();
+    const directive = new DrawWindowDirective(null, null);
     expect(directive).toBeTruthy();
   });
 });
